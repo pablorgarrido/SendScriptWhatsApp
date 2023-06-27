c2fe2484f9aaef3dc859deb24d4c8ea3013865a8
Pablo Garrido <pablorgarrido@gmail.com>
2023-06-27 12:28:34 -0300

    Update beeMovieSendScript.js
    loop infinito

--- beeMovieSendScript.js
+++ beeMovieSendScript.js
@@ -4,19 +4,21 @@ async function enviarScript(scriptText){
 	textarea = main.querySelector(`div[contenteditable="true"]`)
 	
 	if(!textarea) throw new Error("Não há uma conversa aberta")
-	
-	for(const line of lines){
-		console.log(line)
-	
-		textarea.focus();
-		document.execCommand('insertText', false, line);
-		textarea.dispatchEvent(new Event('change', {bubbles: true}));
-	
-		setTimeout(() => {
-			(main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
-		}, 100);
+
+	while (true) {
+		for(const line of lines){
+			console.log(line)
 		
-		if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
+			textarea.focus();
+			document.execCommand('insertText', false, line);
+			textarea.dispatchEvent(new Event('change', {bubbles: true}));
+		
+			setTimeout(() => {
+				(main.querySelector(`[data-testid="send"]`) || main.querySelector(`[data-icon="send"]`)).click();
+			}, 100);
+			
+			if(lines.indexOf(line) !== lines.length - 1) await new Promise(resolve => setTimeout(resolve, 250));
+		}
 	}
 	
 	return lines.length;
